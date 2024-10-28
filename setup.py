3f564526535c343030cdc0bfbdd4261584789e93
Ernesto B <ernestobravc@gmail.com>
2024-10-28 03:14:38 -0600

    structure of the project created

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import find_packages, setup
 
+
 def get_reqs(requirements_path):
     'Return a list of requirements from the requirements text file'
-    
+
     with open(requirements_path, 'r') as file:
         reqs=file.readlines()
         reqs=[req.replace('\n','') for req in reqs if req!='-e .']
     return reqs 
+
 
 setup(
     name='brain_tumor_mri_tfx',
